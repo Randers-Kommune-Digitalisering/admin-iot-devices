0f16ab7b72834c3b37f79d9969930406c84798ca
Søren Thomassen <115066094+st-randers@users.noreply.github.com>
2024-04-03 13:28:36 +0000

    Added convert-units npm module Added test scripts for unit conversion

---
+++ node-red/flows_js/function.a8d56685b22584e1.flows.js
@@ -0,0 +1,51 @@
+const Node = {
+  "id": "a8d56685b22584e1",
+  "type": "function",
+  "z": "1b1f52cac1d78ec2",
+  "name": "Convertible?",
+  "func": "",
+  "outputs": 1,
+  "noerr": 0,
+  "initialize": "",
+  "finalize": "",
+  "libs": [
+    {
+      "var": "convert",
+      "module": "convert-units"
+    }
+  ],
+  "x": 590,
+  "y": 880,
+  "wires": [
+    [
+      "b2887d435b12c492"
+    ]
+  ]
+}
+
+Node.func = async function (node, msg, RED, context, flow, global, env, util, convert) {
+  msg.payload = Array.isArray(msg.payload) ? msg.payload[0] : msg.payload;
+  
+  // True if units are the same
+  
+  if (msg.payload.inputenhed === msg.payload.enhed)
+      msg.payload = true;
+  
+  else
+  try
+  {
+      // Try converting 1 unit
+  
+      msg.payload = convert(1).from(msg.payload.inputenhed).to(msg.payload.enhed);
+      msg.payload = true;
+  }
+  catch (error)
+  {
+      msg.error = error;
+      msg.payload = false;
+  }
+  
+  return msg;
+}
+
+module.exports = Node;
---
+++ node-red/flows_js/http_request.68803ed50f140c9e.flows.js
@@ -0,0 +1,26 @@
+const Node = {
+  "id": "68803ed50f140c9e",
+  "type": "http request",
+  "z": "1b1f52cac1d78ec2",
+  "name": "",
+  "method": "GET",
+  "ret": "obj",
+  "paytoqs": "ignore",
+  "url": "localhost:1880/devices/1/measurements",
+  "tls": "",
+  "persist": false,
+  "proxy": "",
+  "insecureHTTPParser": false,
+  "authType": "",
+  "senderr": false,
+  "headers": [],
+  "x": 390,
+  "y": 780,
+  "wires": [
+    [
+      "23c270ac095889d9"
+    ]
+  ]
+}
+
+module.exports = Node;
